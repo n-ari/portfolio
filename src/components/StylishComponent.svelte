deb5be84122e411a5886cb7585b9b31243c09b67
n-ari <fukunari.r.aa@m.titech.ac.jp>
2020-03-23 00:34:05 +0900

    modify Rating component

---
+++ src/components/StylishComponent.svelte
@@ -0,0 +1,78 @@
+<style>
+    :root {
+        --w: 0.6em;
+        --w2: calc(var(--w) * 0.5);
+        --h: 2.2em;
+        --h2: calc(var(--h) * 0.5);
+    }
+
+    .outer-box {
+        position: relative;
+        border-bottom: solid 2px black;
+        height: var(--h);
+        margin: 0.3em;
+    }
+
+    .blue-arrow::before {
+        content: '';
+        position: absolute;
+        left: calc(var(--w) * -2 + 0.1px);
+        top: calc(var(--h) * -0.5);
+        border-left: solid var(--w) transparent;
+        border-top: solid var(--h2) #25f2f1;
+        border-bottom: solid var(--h2) #25f2f1;
+    }
+    .blue-arrow {
+        position: absolute;
+        left: 1em;
+        border-left: solid var(--w) #25f2f1;
+        border-top: solid var(--h2) transparent;
+        border-bottom: solid var(--h2) transparent;
+    }
+
+    .black-arrow::before {
+        content: '';
+        position: absolute;
+        left: calc(var(--w2) * -1 - 0.1px);
+        border-left: solid var(--w) white;
+        border-top: solid var(--h2) transparent;
+        border-bottom: solid var(--h2) transparent;
+        z-index: 1;
+    }
+    .black-arrow {
+        position: absolute;
+        left: 0em;
+        height: var(--h);
+        border-left: solid var(--w2) black;
+    }
+    .black-arrow::after {
+        content: '';
+        position: absolute;
+        left: -0.1px;
+        border-left: solid var(--w) black;
+        border-top: solid var(--h2) transparent;
+        border-bottom: solid var(--h2) transparent;
+    }
+
+    .content {
+        padding: 0;
+        margin: 0;
+        padding-left: calc(var(--w) * 2.7);
+    }
+    :global(.content > p) {
+        font-size: var(--h2);
+        line-height: 1;
+    }
+    :global(.content *) {
+        margin: 0;
+        padding: 0;
+    }
+</style>
+
+<div class="outer-box">
+    <div class="black-arrow"></div>
+    <div class="blue-arrow"></div>
+    <div class="content">
+        <slot></slot>
+    </div>
+</div>
--- src/components/about/Rating.svelte
+++ src/components/about/Rating.svelte
@@ -1,50 +1,27 @@
+<script>
+    import StylishComponent from "../StylishComponent.svelte";
+</script>
+
 <style>
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	ul > li {
-		display: block;
-		float: left;
-		padding: 0.5em 0.5em;
-        margin: 0.3em 0.2em;
-        border: solid 1px #999999;
-        border-radius: 0.3em;
-
-        /* text-align: center; */
-        font-size: 0.8em;
+    #ratings {
+        display: flex;
+        margin-left: 2em;
     }
-    
+
+    a {
+        text-decoration: none;
+    }
     a::after {
         content: none;
     }
 
-    a.ac-red {
-        text-decoration: none;
-    }
     .ac-red {
         color: red;
         font-weight: bold;
     }
-
-    a.tc-red {
-        text-decoration: none;
-    }
     .tc-red {
         color: rgb(239, 58, 58);
         font-weight: bold;
-    }
-
-    a.cf-red {
-        text-decoration: none;
     }
     .cf-red {
         color: red;
@@ -53,18 +30,16 @@
 </style>
 
 <div id="ratings">
-    <ul>
-        <li>
-            AtCoder: <a href="https://atcoder.jp/users/rickytheta" class="ac-red" target="_blank" rel="nofollow noopener">rickytheta</a><br>
-            Rating: <span class="ac-red">2807</span> (highest: <span class="ac-red">2866</span>)
-        </li>
-        <li>
-            TopCoder: <a href="https://www.topcoder.com/members/rickytheta/details/?track=DATA_SCIENCE&subTrack=SRM" class="tc-red" target="_blank" rel="nofollow noopener">rickytheta</a><br>
-            Rating: <span class="tc-red">2411</span> (highest: <span class="tc-red">2418</span>)
-        </li>
-        <li>
-            CodeForces: <a href="http://codeforces.com/profile/rickytheta" class="cf-red" target="_blank" rel="nofollow noopener">rickytheta</a><br>
-            Rating: <span class="cf-red">2444</span> (highest: <span class="cf-red">2444</span>)
-        </li>
-    </ul>
+    <StylishComponent>
+        <p><a href="https://atcoder.jp/users/rickytheta" target="_blank" rel="nofollow noopener">AtCoder</a></p>
+        <p><a href="https://atcoder.jp/users/rickytheta" target="_blank" rel="nofollow noopener"><span class="ac-red">2807</span></a></p>
+    </StylishComponent>
+    <StylishComponent>
+        <p><a href="https://www.topcoder.com/members/rickytheta/details/?track=DATA_SCIENCE&subTrack=SRM" target="_blank" rel="nofollow noopener">TopCoder</a></p>
+        <p><a href="https://www.topcoder.com/members/rickytheta/details/?track=DATA_SCIENCE&subTrack=SRM" target="_blank" rel="nofollow noopener"><span class="tc-red">2411</span></a></p>
+    </StylishComponent>
+    <StylishComponent>
+        <p><a href="http://codeforces.com/profile/rickytheta" target="_blank" rel="nofollow noopener">CodeForces</a></p>
+        <p><a href="http://codeforces.com/profile/rickytheta" target="_blank" rel="nofollow noopener"><span class="cf-red">2444</span></a></p>
+    </StylishComponent>
 </div>
